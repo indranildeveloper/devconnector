0b8cbc8ce17317c93cd929195d743cd785aca664
Indranil Halder <indranilhalder.dev@gmail.com>
2022-07-24 13:26:35 +0530

    Add and delete education

--- models/Profile.js
+++ models/Profile.js
@@ -70,7 +70,7 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
-      fieldsofstudy: {
+      fieldofstudy: {
         type: String,
         required: true,
       },
