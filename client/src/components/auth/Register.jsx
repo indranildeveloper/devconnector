e11b9f11d0232270b76fa7f53e1ae6a329060d24
Indranil Halder <indranilhalder.dev@gmail.com>
2022-08-02 13:47:49 +0530

    Alert Reducer

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { setAlert } from "../../actions/alert";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +21,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match!", "danger");
     } else {
       console.log("Success");
     }
@@ -101,4 +104,9 @@ const Register = () => {
     </>
   );
 };
-export default Register;
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
